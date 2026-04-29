b11e1f1daf7afa829289a615d8d857521c6d0c56
Chase <cmeyers9@vols.utk.edu>
2026-04-29 15:21:30 -0400

    Implemented full logic of vector comparison and formatted printing to the screen

--- hangman_game.cpp
+++ hangman_game.cpp
@@ -6,12 +6,43 @@
 #include <string>
 #include <vector>
 #include <ostream>
+#include <algorithm>
+#include <cctype>
 #include "hangman.h"
 
 using namespace std;
 
+bool areVectorsEqualIgnoreCase(const vector<char> &v1, const vector<char> &v2, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (tolower(v1[i]) != tolower(v2[i]))
+        {
+            return false; // Mismatch found
+        }
+    }
+    return true; // All elements match ignore case
+}
+
+bool isLetterFound(char letter, const vector<char> &v1, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (tolower(v1[i]) == tolower(letter))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
+    bool game_end = false;
+    char letter_guess;
+    string phrase_guess;
+    int guess_size;
+    vector<char> vphrase_guess;
     string phrase = "";
     cout << "Enter a phrase: ";
     getline(cin, phrase);
@@ -52,5 +83,61 @@ int main()
     }
     cout << endl;
 
+    while (!game_end)
+    {
+        cout << "Guess a letter (type 0 - guess the phrase): ";
+        // implement a throw exception if user doesn't enter a char (letter_guess data type) or 0
+        cin >> letter_guess;
+
+        if (letter_guess == 48)
+        {
+            cout << "Guess the phrase: ";
+            cin >> phrase_guess;
+            guess_size = phrase_guess.length();
+
+            for (int i = 0; i < guess_size; i++)
+            {
+                guessing_phrase.push_back(tolower(phrase_guess[i]));
+            }
+
+            if (areVectorsEqualIgnoreCase(vphrase, guessing_phrase, size))
+            {
+                cout << "Guessing Player won! The Phrase was correctly guessed." << endl;
+                return 0;
+            }
+            else
+            {
+                cout << "Guessing Player lost. The phrase was incorrectly guessed." << endl;
+                return 0;
+            }
+        }
+
+        if (isLetterFound(letter_guess, vphrase, size)) // if guess letter is found
+        {
+            cout << "The guessed letter was found! :)" << endl;
+            for (int i = 0; i < size; i++)
+            {
+                if (tolower(vphrase[i]) == tolower(letter_guess))
+                {
+                    guessing_phrase[i] = vphrase[i];
+                }
+            }
+        }
+        else
+        {
+            cout << "The guessed letter was NOT found. :(" << endl;
+        }
+
+        if (areVectorsEqualIgnoreCase(vphrase, guessing_phrase, size))
+        {
+            cout << "Guessing Player has Won!" << endl;
+            return 0;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            cout << guessing_phrase[i];
+        }
+        cout << endl;
+    }
     return 0;
 }
